b81f33811bade32301b2905347b6fa795913ffac
alizamir25 <ali0786zamir@gmail.com>
2025-02-23 15:59:58 +0000

    patch

--- packages/airtable/src/utils/generateFilter.ts
+++ packages/airtable/src/utils/generateFilter.ts
@@ -4,7 +4,6 @@ import { generateFilterFormula } from "./generateFilterFormula";
 
 export const generateFilter = (filters?: CrudFilters): string | undefined => {
   if (filters) {
-    // Top-level array has an implicit AND as per CRUDFilter design - https://refine.dev/docs/guides-and-concepts/data-provider/handling-filters/#logicalfilters
     return compile(["AND", ...generateFilterFormula(filters)]);
   }
 
